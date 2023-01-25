3652b8b489835956394ccd19742625482ad1e3f3
marcelrm11 <marcelrm11@gmail.com>
2023-01-25 20:34:32 +0000

    access token

--- src/api/routes.py
+++ src/api/routes.py
@@ -47,10 +47,10 @@ def create_user():
 def login():
     email = request.json.get("email", None)
     password = request.json.get("password", None)
-    user = User.query.filter_by(email=email, password=password)
+    user = User.query.filter_by(email=email, password=password).first()
 
-    if username != "test" or password != "test":
+    if not user:
         return jsonify({"msg": "Bad username or password"}), 401
 
-    access_token = create_access_token(identity=username)
+    access_token = create_access_token(identity=email)
     return jsonify(access_token=access_token) 
