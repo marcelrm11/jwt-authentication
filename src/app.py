d024748e1f18b7b3cf11aa6f1f0f16a2be4c429f
marcelrm11 <marcelrm11@gmail.com>
2023-01-31 18:38:54 +0000

    token and get user endpoints

--- src/app.py
+++ src/app.py
@@ -11,18 +11,16 @@ from api.models import db
 from api.routes import api
 from api.admin import setup_admin
 from api.commands import setup_commands
-from flask_jwt_extended import JWTManager
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 
-
-# from models import Person
-
+from api.models import User 
 ENV = os.getenv("FLASK_ENV")
 static_file_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../public/')
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 # Setup the Flask-JWT-Extended extension
-app.config["JWT_SECRET_KEY"] = "super-secret"  # Change this!
+app.config["JWT_SECRET_KEY"] = "my-secret"  # Change this!
 jwt = JWTManager(app)
 
 # database configuration
@@ -52,6 +50,29 @@ app.register_blueprint(api, url_prefix='/api')
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
+
+# Endpoints
+# ------------------------------------------------------
+# token generator
+@app.route('/token', methods=['POST'])
+def create_token():
+    email = 'test@mail.my'
+    # request.json.get('email', None)
+    password = 'dontLook'
+    # request.json.get('password', None)
+    user = User.query.filter_by(email=email, password=password).first()
+    if user is None:
+        return jsonify({'error': 'wrong credentials'}), 401
+    access_token = create_access_token(identity=user.id)
+    return jsonify({'token': access_token, 'user_id': user.id})
+
+# get user
+@app.route('/user', methods=['GET'])
+@jwt_required()
+def private_user():
+    current_user_id = get_jwt_identity()
+    user = User.query.get(current_user_id)
+    return jsonify({'id': user.id, 'email': user.email}), 200
 
 # generate sitemap with all your endpoints
 @app.route('/')
